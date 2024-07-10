57842bd8815ca5085fee46a059773b54ac6380c3
Evseredyn <zhenyavn@gmail.com>
2024-07-10 16:46:29 +0300

    finish section questions

--- js/questions.js
+++ js/questions.js
@@ -1,20 +1,58 @@
 const questions = document.querySelectorAll('.questions__item');
 
+function rotateIcon(icon, isOpen) {
+  icon.style.transform = isOpen ? 'rotate(-90deg)' : '';
+}
+
+function closeAllAnswers() {
+  questions.forEach(q => {
+    const ans = q.querySelector('.questions__answer');
+    const icn = q.querySelector('.questions__icon');
+    ans.classList.remove('open');
+    rotateIcon(icn, false);
+  });
+}
+
+questions.forEach(question => {
+  const header = question.querySelector('.questions__inner');
+  const answer = question.querySelector('.questions__answer');
+  const icon = question.querySelector('.questions__icon');
+
+  // Перевіряємо початковий стан і відповідно обертаємо іконку
+  if (answer.classList.contains('open')) {
+    rotateIcon(icon, true);
+  }
+
+  // Водночас відкриваємо лише одну відповідь
+  header.addEventListener('click', () => {
+    closeAllAnswers(); // Закриваємо всі відповіді перед відкриттям нової
+    
+    const isOpen = !answer.classList.contains('open'); // Перевіряємо новий стан
+    answer.classList.toggle('open');
+    rotateIcon(icon, isOpen);
+  });
+});
+
+// const questions = document.querySelectorAll('.questions__item');
+
+// questions.forEach(question => {
+//   const header = question.querySelector('.questions__inner');
+//   const answer = question.querySelector('.questions__answer');
+
+//   header.addEventListener('click', () => {
+//     // closeAllAnswers(); // Закриваємо всі відповіді
+//     answer.classList.toggle('open'); // Відкриваємо/закриваємо поточну відповідь
+//   });
+
+// });
+
+
 // function closeAllAnswers() {
 //   questionItems.forEach(item => {
 //     const answer = item.querySelector('.questions__answer');
 //     answer.classList.remove('open');
 //   });
 // }
-
-questions.forEach(question => {
-  const header = question.querySelector('.questions__inner');
-  const answer = question.querySelector('.questions__answer');
-
-  header.addEventListener('click', () => {
-    // closeAllAnswers(); // Закриваємо всі відповіді
-    answer.classList.toggle('open'); // Відкриваємо/закриваємо поточну відповідь
-  });
 
   // header.addEventListener('click', () => {
   //   if (answer.classList.contains('open')) {
@@ -26,4 +64,3 @@ questions.forEach(question => {
   //     answer.classList.add('open');
   //   }
   // });
-});
