a41cfbd0fb669b1a8cf1b12f2285dc726e91f4eb
Yaroslav Plaksii <yaroslav.plaksii@gmail.com>
2024-07-21 17:51:23 +0300

    Added news block show/hide, added click for button for next 3 news

--- js/news.js
+++ js/news.js
@@ -1,37 +1,40 @@
-let buttonBlock = document.getElementsByClassName('news__icon');
-let titleBlock = document.getElementsByClassName('news__title');
+$(document).ready(function() {
+    $('.news__inner').on('click', function() {
+       let newsAnswer = $(this).parent().find('.news__answer');
+       let mainBlock = $(this).parent().parent().find('.news__thumb');
+       let bpSize = (screen.availWidth >= 320 && screen.availWidth <= 429) ? true : false;  
+       let newsImg = $(this).parent().parent().find('.news__img');       
+       if (newsAnswer.hasClass('open')) {          
+           newsAnswer.removeClass('open');
+           if (!bpSize) {
+            newsImg.css('width', 120 + 'px');
+            newsImg.css('height', 120 + 'px');
+           }
+           if (bpSize) {
+            mainBlock.css('display', 'block');
+            $(this).find('.news__circ').css('margin-top', '0px');
+            $(this).find('.news__title').css({'margin-top': '0px', 'width': '170px','margin-left': '55px'});
+           }           
+       } else {
+           newsAnswer.addClass('open');
+           if (!bpSize) {
+            newsImg.css('width', 180 + 'px');
+            newsImg.css('height', 180 + 'px');
+           }
+           if (bpSize) {
+            mainBlock.css({'display': 'flex','width': screen.availWidth + 'px', 'margin-left': '-55px'});
+            $(this).find('.news__circ').css('margin-top', '-46px');
+            $(this).find('.news__title').css({'margin-top': '65px', 'width': '90%','margin-left': '-10px'});
+           }
+       }
+    });
 
-for (let i=0; i<buttonBlock.length; i++) {
-    buttonBlock[i].addEventListener('click', function(e) {
-        setPositionForBlock(e.target);   
+    $('a.news__link.link').on('click', function(e) {
+        e.preventDefault();
+        $('li.news__item').each(function(){
+            if($(this).css('display') == 'none') {
+                $(this).removeAttr('style');
+            }
+        });
     });
-}
-for (let i=0; i<titleBlock.length; i++) {
-    titleBlock[i].addEventListener('click', function(e) {
-        setPositionForBlock(e.target);   
-    });
-}
-function setPositionForBlock(elem) {console.log(elem);
-    let blockStatus = 1;
-    let bpSize = (screen.availWidth >= 320 && screen.availWidth <= 429) ? 0 : 60;          
-    let selectedBlock = elem.parentNode.parentNode.parentNode;
-    let isOpen = elem.parentNode.parentNode.getElementsByClassName('news__answer')[0];
-    if(isOpen.classList.contains('open')){
-        blockStatus *= (-1);
-        if (bpSize == 0) {
-            selectedBlock.style.display = 'flex';
-            elem.style.marginTop = '0px';
-            elem.parentNode.getElementsByClassName('news__title')[0].removeAttribute('style');
-        }
-    } else {
-        if (bpSize == 0) {
-            selectedBlock.style.display = 'block';
-            elem.style.marginTop = '-120px';
-            elem.parentNode.getElementsByClassName('news__title')[0].style.width = '282px';
-        }
-    }
-    let w = selectedBlock.querySelector('.news__img').width;
-    let h = selectedBlock.querySelector('.news__img').height;
-    selectedBlock.querySelector('.news__img').style.width = w + (bpSize * blockStatus) + 'px';
-    selectedBlock.querySelector('.news__img').style.height = h + (bpSize * blockStatus) + 'px';
-}
+});
